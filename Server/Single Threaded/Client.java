7d47496582f0a81036780ae6dc6b97affe5289b5
SameerMatoria <111170429+SameerMatoria@users.noreply.github.com>
2025-03-19 00:23:10 +0530

    Add files via upload

---
+++ Server/Single Threaded/Client.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    public void run() throws IOException,UnknownHostException {
+        int port = 8010;
+        InetAddress address = InetAddress.getByName("localhost");
+        System.out.println(address);
+        Socket socket = new Socket(address,port);
+        PrintWriter toServer = new PrintWriter(socket.getOutputStream());
+        BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toServer.println("Hello from the Client");
+        toServer.flush();
+        String line = fromServer.readLine();
+        System.out.println("Responose from the server is :" +line);
+        toServer.close();
+        fromServer.close();
+        socket.close();
+    }
+    public static void main(String[] args){
+        Client client = new Client();
+        try {
+            client.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
